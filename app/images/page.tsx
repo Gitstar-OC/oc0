cee7c2532ef2171d5ed58c9b7b6384487cb04468
OC <chandankarom07@gmail.com>
2025-06-03 13:36:56 +0530

    Started to work on the images now

---
+++ app/images/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {    
+    return (
+        <div>
+            <h1>Image Page</h1>
+            <p>This page is dedicated to displaying images.</p>
+        </div>                                                  
+    )
+}
